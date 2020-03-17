0f35ce4b8b1c122ba6ea1a640971b46c1b353da4
Juan <juanpmiranda92@gmail.com>
2020-03-17 00:52:27 -0300

    appearence

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-var header = document.querySelector("header");
-var menuItems = document.querySelectorAll("li");
-console.log(menuItems[0]);
+var btnUp = document.getElementById('btn-up')
+var burger = document.getElementById('burger');
 
 window.onscroll = () => {
-    if (window.pageYOffset >= header.offsetTop + 300) {
-        header.classList.add("sticky");
-        menuItems.classList.add("sticky-li-adjust");
+    if (window.pageYOffset >= 350) {
+        btnUp.style.display = 'block';
+        btnUp.style.animation = 'FadeIn 1.5s 1';
     } else {
-        header.classList.remove("sticky");
-        menuItems.classList.add("sticky-li-adjust");
+        btnUp.style.animation = 'FadeOut 1.5s 1';
+        setInterval(() => {
+            // btnUp.style.display = 'none';
+        }, 1500);
     }
+}
+
+function ShowMenu () {
+    burger.style.rotate = 90;
 }
