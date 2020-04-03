3c72a3554120c58acb6b97be3b01e8626c1d76a0
Juan <juanpmiranda92@gmail.com>
2020-04-02 21:28:26 -0300

    new layout

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
 var btnUp = document.getElementById('btn-up')
 var burger = document.getElementById('burger');
+var modal = document.getElementById('contacto');
+
+window.onload = () => {
+    
+}
 
 window.onscroll = () => {
     if (window.pageYOffset >= 350) {
         btnUp.style.display = 'block';
-        btnUp.style.animation = 'FadeIn 1.5s 1';
+        btnUp.style.animation = 'FadeIn 1s 1';
     } else {
-        btnUp.style.animation = 'FadeOut 1.5s 1';
-        setInterval(() => {
-            // btnUp.style.display = 'none';
-        }, 1500);
+        btnUp.style.animation = 'FadeOut 0.7s 1';
+        btnUp.style.animationFillMode = 'forwards';
     }
 }
 
-function ShowMenu () {
-    burger.style.rotate = 90;
+window.onclick = (event) => {
+    if(event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
+
+function ScrollUp() {
+    window.scrollTo(0, 0);
+}
+
+function ShowModal() {
+    modal.style.display = 'block';
+    return false;
+}
+
+function CloseModal() {
+
 }
