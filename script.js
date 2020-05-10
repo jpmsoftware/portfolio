49c304098a1067ee4bfdd648fa4e3e3109871a09
Juan <juanpmiranda92@gmail.com>
2020-05-09 21:14:36 -0300

    more work

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-var header = document.getElementsByTagName('header')[0];
-var logo = document.querySelector('nav > a');
-var menuItems = document.querySelectorAll('nav ul li a');
-var lastScroll = 0;
+var proyectos = document.getElementById("proyectos");
+var currentPos = 0;
 
+window.onscroll = () => {
+    console.clear();
+    console.log(proyectos.offsetTop);
+    currentPos = window.scrollY;
+    console.log(currentPos);
 
-
-function IsScrollingUp(pLast, pCurrent) {
-    if(pLast < pCurrent) {
-        return false;
-    }
-    else {
-        return true;
+    if(currentPos >= proyectos.offsetTop) {
+        proyectos.style.visibility = "visible";
+        proyectos.style.animation = "FadeIn 1s 1";
     }
 }
