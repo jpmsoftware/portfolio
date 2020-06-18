567195c91b5977b6e55a545fd3a8d8273091e6fa
Juan <juanpmiranda92@gmail.com>
2020-06-18 19:42:03 -0300

    updated content, worked on buttons

--- action.js
+++ action.js
@@ -2,13 +2,14 @@ var proyectos = document.getElementById("proyectos");
 var currentPos = 0;
 
 window.onscroll = () => {
-    console.clear();
-    console.log(proyectos.offsetTop);
     currentPos = window.scrollY;
-    console.log(currentPos);
-
+    
     if(currentPos >= proyectos.offsetTop) {
         proyectos.style.visibility = "visible";
         proyectos.style.animation = "FadeIn 1s 1";
     }
+}
+
+function Sliders() {
+
 }
