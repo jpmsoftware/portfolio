3b0cb8685ae30a158eb998eb718bac21a8fda646
Juan <juanpmiranda92@gmail.com>
2020-07-10 18:14:51 -0300

    some mods

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,9 @@
+//Get DOM elements
+var burger = document.getElementById('burger');
+
+burger.addEventListener('click', toggleMenu, false);
+
+
 window.onload = () => {
     markCurrentPage();
 }
@@ -6,11 +12,6 @@ markCurrentPage = () => {
     const menuItems = document.getElementsByClassName('menu-item');
     const currentPage = document.getElementsByTagName('title')[0].innerHTML;
     
-    //reset previous
-    for(var i = 0; i < menuItems.length; i++) {
-        menuItems[i].style.backgroundColor = 'transparent';
-    }
-
     for(var i = 0; i < menuItems.length; i++) {
         if(menuItems[i].lastElementChild.innerHTML == currentPage) {
             menuItems[i].style.backgroundColor = '#3A63FF';
@@ -19,5 +20,4 @@ markCurrentPage = () => {
 }
 
 function toggleMenu() {
-    console.log('test');
 }
