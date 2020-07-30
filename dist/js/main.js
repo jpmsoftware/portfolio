f0d393b5b3b18f949c1d487f5882eb42dcda67b5
Juan <juanpmiranda92@gmail.com>
2020-07-29 23:11:33 -0300

    section acerca

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,8 +3,8 @@ const btnProyectos = document.getElementById('button-proyectos');
 
 menu[0].addEventListener('click', showHome);
 menu[1].addEventListener('click', showProyectos);
-menu[1].addEventListener('click', showAcerca);
-menu[1].addEventListener('click', showContacto);
+menu[2].addEventListener('click', showAcerca);
+menu[3].addEventListener('click', showContacto);
 btnProyectos.addEventListener('click', showProyectos);
 
 function showHome() {
@@ -16,9 +16,13 @@ function showProyectos() {
 }
 
 function showAcerca() {
-    
+    window.scrollTo(0, 2000);
 }
 
 function showContacto() {
     
+}
+
+window.onscroll = () => {
+    console.log(window.scrollY);
 }
