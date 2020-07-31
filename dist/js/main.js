37052101d088b31ffa0e3333d247c672c8f46a53
Juan <juanpmiranda92@gmail.com>
2020-07-31 14:51:53 -0300

    sticky nav bar

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,29 +1,27 @@
+//DOM ELEMENTS
+const header = document.getElementsByTagName('header')[0];
 const menu = document.getElementsByClassName('menu-item');
 const btnProyectos = document.getElementById('button-proyectos');
 const btnEnviar = document.getElementById('enviar');
+var sectionsCoordinates;
 var lastScroll = 0;
 
-menu[0].addEventListener('click', showHome);
-menu[1].addEventListener('click', showProyectos);
-menu[2].addEventListener('click', showAcerca);
-menu[3].addEventListener('click', showContacto);
-btnProyectos.addEventListener('click', showProyectos);
-btnEnviar.addEventListener('click', showSuccess);
+//EVENTS
+menu[0].addEventListener('click', () => window.scrollTo(0, sectionsCoordinates.home));
+menu[1].addEventListener('click', () => window.scrollTo(0, sectionsCoordinates.proyectos));
+menu[2].addEventListener('click', () => window.scrollTo(0, sectionsCoordinates.acerca));
+menu[3].addEventListener('click', () => window.scrollTo(0, sectionsCoordinates.contacto));
+btnProyectos.addEventListener('click', () => window.scrollTo(0, sectionsCoordinates.proyectos));
+btnEnviar.addEventListener('click', () => document.getElementById('success').style.display = 'block');
 
-function showHome() {
-    window.scrollTo(0, 0);
-}
-
-function showProyectos() {
-    window.scrollTo(0, 1000);
-}
-
-function showAcerca() {
-    window.scrollTo(0, 2000);
-}
-
-function showContacto() {
-    window.scrollTo(0, 3000);
+window.onload = () => {
+    //GET SECTIONS COORDINATES
+    sectionsCoordinates = {
+        home: document.getElementById('home').offsetTop,
+        proyectos: document.getElementById('proyectos').offsetTop,
+        acerca: document.getElementById('acerca').offsetTop,
+        contacto: document.getElementById('contacto').offsetTop
+    }
 }
 
 function isScrollingUp(before, after) {
@@ -34,13 +32,16 @@ function isScrollingUp(before, after) {
     }
 }
 
-function showSuccess() {
-    document.getElementById('success').style.display = 'block'
-}
-
 window.onscroll = () => {
     if (isScrollingUp(lastScroll, window.scrollY)) {
-        console.log("UP");
+        if (window.scrollY >= 200) {
+            header.classList.add('sticky');
+        } else {
+            header.classList.remove('sticky');
+        }
+    } else {
+        header.classList.remove('sticky');
     }
     lastScroll = window.scrollY;
 }
+
