db56a0c7e67537db65f25a0b7ef64ca9cc09d824
Juan <juanpmiranda92@gmail.com>
2021-02-11 19:49:16 -0300

    updated links and screenshot

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,27 +1,16 @@
 const menu = document.querySelector('.menu');
-const btnEnviar = document.querySelector('#enviar');
 var sections = {};
 
 menu.addEventListener('click', (e) => { MenuScroll(e) })
 
-// Get sections coordinates
 window.onload = () => {
+    // Get sections coordinates
     sections = {
         home: document.querySelector('.home').offsetTop,
         proyectos: document.querySelector('.proyectos').offsetTop,
         acerca: document.querySelector('.acerca').offsetTop,
         contacto: document.querySelector('.contacto').offsetTop
     }
-}
-
-btnEnviar.onclick = () => {
-    const forms = document.querySelectorAll('#form-name, #form-email, #form-msg');
-    window.setTimeout(() => {
-        forms.forEach((element) => {
-            element.value = '';
-        });
-    }, 1000);
-    alert('Gracias por su mensaje');
 }
 
 function MenuScroll(e) {
